a8824a9681969c28883f801e29ab8b990edacf5b
danielsobisz <dsobisz97@gmail.com>
2020-11-24 10:56:57 +0100

    Updates

---
+++ precache-manifest.0018460d635eda9bee70e137e1555fb4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b783c593d5c5b52b3d3280d7e3dcd",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "0b1447aba8cc5238e19d",
+    "url": "/myportfolio/static/css/main.285bb511.chunk.css"
+  },
+  {
+    "revision": "eb9cbf1c43e5c67154df",
+    "url": "/myportfolio/static/js/2.ca13b6cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.ca13b6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1447aba8cc5238e19d",
+    "url": "/myportfolio/static/js/main.2721e1c5.chunk.js"
+  },
+  {
+    "revision": "27ff665cd96372b8b66a",
+    "url": "/myportfolio/static/js/runtime-main.047ac6ea.js"
+  },
+  {
+    "revision": "bc202eea2a28b30bd115fa2a6bd8f063",
+    "url": "/myportfolio/static/media/background-main-md.bc202eea.jpg"
+  },
+  {
+    "revision": "fdf4d71db79d3d37c7ac74df66f301a6",
+    "url": "/myportfolio/static/media/background-main-sm.fdf4d71d.jpg"
+  },
+  {
+    "revision": "833bbfcb2c1a840edfa2b04b19c7ce14",
+    "url": "/myportfolio/static/media/background-main.833bbfcb.jpg"
+  },
+  {
+    "revision": "e86b571afa355bf094fe0d7d6743d880",
+    "url": "/myportfolio/static/media/compare-sm.e86b571a.png"
+  },
+  {
+    "revision": "919695f8a068ff3ed9a93e723b95da38",
+    "url": "/myportfolio/static/media/compare.919695f8.PNG"
+  },
+  {
+    "revision": "83995ec55124b665b09a27e896bcc271",
+    "url": "/myportfolio/static/media/frontproject-sm.83995ec5.png"
+  },
+  {
+    "revision": "c62cfe1d15c1cbefc3fdb294980e2134",
+    "url": "/myportfolio/static/media/frontproject.c62cfe1d.png"
+  },
+  {
+    "revision": "f5f77b95da66e47a055f7b0f7c5d0941",
+    "url": "/myportfolio/static/media/piracik.f5f77b95.jpg"
+  },
+  {
+    "revision": "759e02bd8fa34c1afd82b7b532f07fd6",
+    "url": "/myportfolio/static/media/soniakuras-sm.759e02bd.png"
+  },
+  {
+    "revision": "3638f79063e923493902098c4a2dd515",
+    "url": "/myportfolio/static/media/soniakuras.3638f790.jpg"
+  },
+  {
+    "revision": "0e07195958903e5588de1aac3e9c7543",
+    "url": "/myportfolio/static/media/weatherchannel-sm.0e071959.png"
+  },
+  {
+    "revision": "b4f706b1879de0562f664f56989428d3",
+    "url": "/myportfolio/static/media/weatherchannel.b4f706b1.png"
+  },
+  {
+    "revision": "6edeb95533b3be238a1aff5f36ce2669",
+    "url": "/myportfolio/static/media/webdev-sm.6edeb955.png"
+  },
+  {
+    "revision": "7ef5734a6d8ff7f6cde50a085489fba7",
+    "url": "/myportfolio/static/media/webdev.7ef5734a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73c8d9587a63ecb0e158146547c29f99.js"
+  "/myportfolio/precache-manifest.0018460d635eda9bee70e137e1555fb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
